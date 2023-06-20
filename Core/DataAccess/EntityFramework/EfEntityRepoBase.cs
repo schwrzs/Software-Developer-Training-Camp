2d3e454e341e90627c092495fd55b1bf00da95c5
Eser <eser.sune@hotmail.com>
2023-06-20 23:56:40 +0300

    Adding Core layer to project

---
+++ Core/DataAccess/EntityFramework/EfEntityRepoBase.cs
@@ -0,0 +1,73 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+   public class EfEntitiyRepoBase<TEntity,TContext> : IEntityRepository<TEntity>
+    where TEntity : class, IEntity,new()
+    where TContext :  DbContext, new()
+
+    {
+        public void Add(TEntity entity)
+        {
+            //for better performance IDıspossable pattern implementation of C#
+            using (TContext context = new TContext())
+            {
+                var addedEntitiy = context.Entry(entity);
+                addedEntitiy.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntitiy = context.Entry(entity);
+                deletedEntitiy.State = EntityState.Deleted;
+                context.SaveChanges();
+
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public List<TEntity> GetAllByProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     // the class that implements IEntity is a database table.
     public interface IEntity
